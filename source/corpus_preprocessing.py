58b247cd4cae5640fadf868079b3c3b15597fd7a
Erich Brendell <erychbrendell@gmail.com>
2024-03-24 16:29:36 -0300

    Code modularization

--- source/corpus_preprocessing.py
+++ source/corpus_preprocessing.py
@@ -72,16 +72,3 @@ def remove_gutenberg_header_footer(text):
     start_index = text.find(start_marker) + len(start_marker)
     end_index = text.find(end_marker)
     return text[start_index:end_index]
-
-
-if __name__ == "__main__":
-    # URL of the book "Moby Dick" on Project Gutenberg
-    url = "https://www.gutenberg.org/files/2701/2701-0.txt"
-    local_filename = "moby_dick.txt"
-    if not os.path.exists(local_filename):
-        download_file(url, local_filename)
-    with open(local_filename, 'r', encoding='utf-8-sig') as f:
-        text_without_preprocessing = f.read()
-    text_without_preprocessing = remove_gutenberg_header_footer(text_without_preprocessing)
-    text_preprocessed = corpus_preprocessing(text_without_preprocessing)
-    print(text_preprocessed)
