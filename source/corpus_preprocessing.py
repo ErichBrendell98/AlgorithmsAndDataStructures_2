86a846bcfbd25c362d016de1abdc92affaafc37c
Erich Brendell <erychbrendell@gmail.com>
2024-03-24 14:16:06 -0300

    renaming the file

--- source/corpus_preprocessing.py
+++ source/corpus_preprocessing.py
@@ -21,19 +21,15 @@ def corpus_preprocessing(text):
     """
     # Convert all text to lowercase
     text = text.lower()
-
     # Remove punctuation and special characters
     text = text.translate(str.maketrans('', '', string.punctuation))
-
     # Splits text into words
     tokens = word_tokenize(text)
-
     # Remove stop words
     stop_words = set(stopwords.words('english'))
     tokens = [token for token in tokens if token not in stop_words]
 
     print(tokens)
-
 
 if __name__ == "__main__":
     text_without_preprocessing = """Welcome to the Programming Knowledge! Lets start with our first tutorial on NLTK. NLTK is a leading platform for building Python programs to work with human language data. It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, along with a suite of text processing libraries for classification, tokenization, stemming, tagging, parsing, and semantic reasoning, wrappers for industrial-strength NLP libraries, and an active discussion forum. Thanks for joining us. Have a great time learning with us."""
