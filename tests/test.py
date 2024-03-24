8d38a5ac323fa994e316f18e9195874ba1950a44
Erich Brendell <erychbrendell@gmail.com>
2024-03-23 21:19:36 -0300

    Introduction to NLTK

---
+++ tests/test.py
@@ -0,0 +1,19 @@
+import nltk
+import matplotlib.pyplot as plt
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.probability import FreqDist
+
+nltk.download('punkt')
+
+text = """Welcome to the Programming Knowledge! Lets start with our first tutorial on NLTK. NLTK is a leading platform for building Python programs to work with human language data. It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, along with a suite of text processing libraries for classification, tokenization, stemming, tagging, parsing, and semantic reasoning, wrappers for industrial-strength NLP libraries, and an active discussion forum. Thanks for joining us. Have a great time learning with us."""
+
+word_tokenize = word_tokenize(text)
+fd = FreqDist(word_tokenize)
+
+# print(word_tokenize(text)) # Tokenizing words
+# print("\n")
+# print(sent_tokenize(text)) # Tokenizing sentences
+print(FreqDist(word_tokenize)) # Frequency Distribution of words
+print(fd.most_common(5)) # Most common words
+fd.plot(30, cumulative=False) # Plotting the frequency distribution of words
+plt.show()
