eb582546cf58535f4302ea94cbc7a10699eaa621
Erich Brendell <erychbrendell@gmail.com>
2024-04-14 21:28:54 -0300

    Creates the Counter class and modifies the reverse_in_order_traversal function to help modify the findKthLargestValue function - time complexity O(h+k)

--- week04/src/challenge02/kth_largest_value.py
+++ week04/src/challenge02/kth_largest_value.py
@@ -1,6 +1,12 @@
 from bst import BST
 
 
+class Counter:
+    def __init__(self):
+        self.count = 0
+        self.value = None
+
+
 def findKthLargestValue(tree, k):
     """
     Finds the kth largest integer in a Binary Search Tree (BST).
@@ -17,21 +23,33 @@ def findKthLargestValue(tree, k):
     Returns:
     int: The kth largest integer present in the BST.
     """
+    counter = Counter()
+    reverse_in_order_traversal(tree.root, k, counter)
+    return counter.value
 
-    # List to store nodes in reverse order
-    nodes = []
 
-    # Helper function to traverse the tree
-    def reverse_in_order_traversal(node):
-        if node is None or len(nodes) >= k:
-            return
-        reverse_in_order_traversal(node.right_child)
-        if len(nodes) < k:
-            nodes.append(node.value)
-        reverse_in_order_traversal(node.left_child)
+def reverse_in_order_traversal(node, k, counter):
+    """
+    Performs a reverse in-order traversal (right, root, left) on a Binary Search Tree (BST).
 
-    # Start crossing
-    reverse_in_order_traversal(tree.root)
+    The function traverses the BST in a reverse in-order manner to collect the node values in a descending ordered list.
+    It then increments a counter each time a node is visited. When the counter is equal to k, the node value is stored.
+    The BST is assumed to contain only integer values.
+    In case of duplicate integers, they are treated as distinct values.
+    The kth largest integer is determined in the context of these distinct values.
 
-    # Return the kth largest value
-    return nodes[-1] if len(nodes) == k else -1
+    Parameters:
+    node: the current node in the BST.
+    k (int): A positive integer representing the kth position.
+    counter (Counter): A counter object that keeps track of the number of nodes visited and the kth largest value.
+
+    Returns:
+    Anything. The kth largest value is stored in the counter object.
+    """
+    if node is None or counter.count >= k:
+        return
+    reverse_in_order_traversal(node.right_child, k, counter)
+    if counter.count < k:
+        counter.count += 1
+        counter.value = node.value
+        reverse_in_order_traversal(node.left_child, k, counter)
